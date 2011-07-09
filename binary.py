4d2a1e88111ab6efda57deaeed84d304e51d86e2
Gennadiy Zlobin <gennad.zlobin@gmail.com>
2011-07-10 00:26:22 +0700

    Added binary.

---
+++ binary.py
@@ -0,0 +1,13 @@
+def binary(seq, x):
+    l = 0
+    r = len(seq)
+    while l < r:
+        m = (l + r) // 2
+        if seq[m] < x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+if __name__ == '__main__':
+    print binary([1,2,3,4,5,6,7,8], 8)
