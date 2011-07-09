1c4c0ae8fbab80fee4535042ecc12e5f17a07c18
Gennadiy Zlobin <gennad.zlobin@gmail.com>
2011-07-09 22:51:33 +0700

    Added dfs, bfs and permutations.

--- graphs.py
+++ graphs.py
@@ -58,3 +58,44 @@ W = [[0,2,1,3,9,4,_,_],
 [_,_,2,_,_,0,2,2],
 [_,_,_,_,_,1,0,6],
 [_,_,_,_,_,9,8,0]]
+
+def recursive_dfs(graph, start, path=[]):
+  """Recursive depth first search from start."""
+  path=path+[start]
+  for node in graph[start]:
+    if not node in path:
+      path=recursive_dfs(graph, node, path)
+  return path
+
+def iterative_dfs(graph, start, path=[]):
+  """Iterative depth first search from start."""
+  q=[start]
+  while q:
+    v=q.pop(0)
+    if v not in path:
+      path=path+[v]
+      q=graph[v]+q
+  return path
+
+def iterative_bfs(graph, start, path=[]):
+  """Iterative breadth first search from start."""
+  q=[start]
+  while q:
+    v=q.pop(0)
+    if not v in path:
+      path=path+[v]
+      q=q+graph[v]
+  return path
+
+"""
+   +---- A
+   |   /   \
+   |  B--D--C
+   |   \ | /
+   +---- E
+"""
+
+graph = {'A':['B','C'],'B':['D','E'],'C':['D','E'],'D':['E'],'E':['A']}
+print 'recursive dfs ', recursive_dfs(graph, 'A')
+print 'iterative dfs ', iterative_dfs(graph, 'A')
+print 'iterative bfs ', iterative_bfs(graph, 'A')
---
+++ permutations.py
@@ -0,0 +1,12 @@
+def all_perms(str):
+    if len(str) <=1:
+        yield str
+    else:
+        for perm in all_perms(str[1:]):
+            for i in range(len(perm)+1):
+                #nb str[0:1] works in both string and list contexts
+                yield perm[:i] + str[0:1] + perm[i:]
+
+if __name__ == '__main__':
+    for i in all_perms([1,2,3]):
+        print i
